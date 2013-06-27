52384aae11ca20ccd7651e41a3acbd509d495232
srivello <srivello@hotmail.com>
2013-06-28 00:51:12 +0700

    EventDispatcher alpha complete

--- projects/UnityWithCocktails/Assets/UnityWithCocktails/Lessons/LessonX2_MOM/TestFromOtherScope.cs
+++ projects/UnityWithCocktails/Assets/UnityWithCocktails/Lessons/LessonX2_MOM/TestFromOtherScope.cs
@@ -7,9 +7,7 @@
  * "Software"), to deal in the Software without restriction, including  
  * without limitation the rights to use, copy, modify, merge, publish,  
  * distribute, sublicense, and#or sell copies of the Software, and to   
- * permit persons to whom the Software is furn
- * 
- * ished to do so, subject to
+ * permit persons to whom the Software is furnished to do so, subject to
  * the following conditions:                                            
  *                                                                      
  * The above copyright notice and this permission notice shall be       
@@ -25,28 +23,30 @@
  */
 // Marks the right margin of code *******************************************************************
 
+
 //--------------------------------------
 //  Imports
 //--------------------------------------
+using UnityEngine;
+using System.Collections;
+using com.rmc.events;
 
 //--------------------------------------
 //  Namespace
 //--------------------------------------
-namespace com.rmc.events
-{
+
 	
 	//--------------------------------------
 	//  Class
 	//--------------------------------------
-	/// <summary>
-	/// IEventDispatcher
-	/// </summary>
-	public interface IEventListener
+	
+	public class TestFromOtherScope : IEventListener, IEventDispatcher
 	{
 	
 		//--------------------------------------
 		//  Properties
 		//--------------------------------------
+		
 		// GETTER / SETTER
 		
 		// PUBLIC
@@ -54,32 +54,90 @@ namespace com.rmc.events
 		// PUBLIC STATIC
 		
 		// PRIVATE
-		
-		// PUBLIC
-		
-		// PUBLIC STATIC
-		
-		// PRIVATE
+		private EventDispatcher eventDispatcher;
+	
+		private EventDelegate onEventDelegate;
 		
 		// PRIVATE STATIC
 		
 		//--------------------------------------
 		//  Methods
-		//--------------------------------------
+		//--------------------------------------		
 		
 		// PUBLIC
+		/// <summary>
+		/// Start this instance.
+		/// </summary>
+		public TestFromOtherScope ()
+		{
 		
+			eventDispatcher = new EventDispatcher ();
+		
+			IEventListener target = this as IEventListener;
+			//
+		
+			/*
+			 * 
+			 * 		NEXT: REMOVE 'IEventListener.HandleEven' AND USE _onCustomEvent, (pass as string? Function? not-delegate somehow--)
+			 * 
+			 * 
+			 * */
+		
+			eventDispatcher.addEventListener 			(target, TestEvent.EVENT_NAME, _onCustomEvent1);
+			eventDispatcher.addEventListener 			(target, TestEvent.EVENT_NAME, _onCustomEvent2);
+			//Debug.Log ("	hasEventListener(): " + 	eventDispatcher.hasEventListener 	(target, TestEvent.EVENT_NAME, _onCustomEvent1));
+			//Debug.Log ("	removeEventListener(): " + 	eventDispatcher.removeEventListener (target, TestEvent.EVENT_NAME, _onCustomEvent1));
+			//Debug.Log ("	hasEventListener(): " + 	eventDispatcher.hasEventListener 	(target, TestEvent.EVENT_NAME, _onCustomEvent1));
+			
+			/*
+				+ void dispatchEventListener(IEvent aIEvent)
+				
+				+ void addEventListener          (string aEventName, IEvent aIEvent, Delegate aDelegate)
+				+ bool removeEventListener       (string aEventName, IEvent aIEvent, Delegate aDelegate)
+				+ bool hasEventListener          (string aEventName, IEvent aIEvent, Delegate aDelegate)
+				+ void removeAllEventListeners   (string aEventName, IEvent aIEvent)
+				+ void removeAllEventListeners   (); //100% are removed
+				
+			*/
+			//_callListenerDelegate (_onCustomEvent );
+		
+			
+		}
+		
+		
+				
+		public void dispatchIt()
+		{
+			eventDispatcher.dispatchEvent (new TestEvent (TestEvent.EVENT_NAME));	
+			
+		}
+	
 		// PUBLIC STATIC
 		
 		// PRIVATE
 		
 		// PRIVATE STATIC
 		
+		
 		//--------------------------------------
 		//  Events
 		//--------------------------------------
-		
-
+		public void _onCustomEvent1 (IEvent iEvent) 
+		{
+			Debug.Log ("	2. _onCustomEvent1(): " + iEvent);
+		}
+	
+	
+	
+		public void _onCustomEvent2 (IEvent iEvent) 
+		{
+			Debug.Log ("	2. _onCustomEvent2(): " + iEvent);
+		}
+	
+	
 	}
-}
+
+	
+
+
 
