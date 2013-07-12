39bb0a710b613c1136c0cc328fbe0bd189d4a516
srivello <srivello@github.com>
2013-07-12 14:27:28 +0700

    Update

--- projects/UnityWithCocktails/Assets/UnityWithCocktails/Lessons/Lesson8_NamingConventions/Scripts/Lesson8_NamingConventions.cs
+++ projects/UnityWithCocktails/Assets/UnityWithCocktails/Lessons/Lesson8_NamingConventions/Scripts/Lesson8_NamingConventions.cs
@@ -76,76 +76,76 @@ namespace sample_namespace
 		///<summary>
 		///	Use this for initialization
 		///</summary>
-void Start () 
-{
-
-//-----------------------------------------------------------------
-//  1. RMC Naming Conventions
-//
-//			(See http://www.rivellomultimediaconsulting.com/c-sharp-coding-conventions-and-standards/ )
-//
-//-----------------------------------------------------------------
-
-// INSTANTIATE
-//1.1
-ClassWithRMCConventions classWithRMCConventions = new ClassWithRMCConventions();	
-
-// STATIC MEMBER
-//1.2
-ClassWithRMCConventions.SamplePublicStaticMethod("foo string1");					
-//1.3
-string s1 = ClassWithRMCConventions.SAMPLE_PUBLIC_STATIC_CONSTANT;					
-
-// INSTANCE MEMBER
-//1.4
-classWithRMCConventions.samplePublicMethod("foo string1");							
-//1.5
-classWithRMCConventions.samplePublicVariable = "foo blah string1";					
-
-
-
-//-----------------------------------------------------------------
-//  2. C# Naming Conventions 	
-//
-//			(See http://msdn.microsoft.com/en-us/library/vstudio/ff926074.aspx)
-//			(See http://msdn.microsoft.com/en-us/library/ms229045.aspx)
-//
-//-----------------------------------------------------------------
-
-// INSTANTIATE
-//2.1
-ClassWithCSharpConventions classWithCSharpConventions = new ClassWithCSharpConventions();	
-
-// STATIC MEMBER
-//2.2
-ClassWithCSharpConventions.SamplePublicStaticMethod("foo string2");							
-//2.3
-string s2 = ClassWithCSharpConventions.SamplePublicStaticConstant;							
-
-// INSTANCE MEMBER
-//2.4
-classWithCSharpConventions.SamplePublicMethod("foo string2");								
-//2.5
-classWithCSharpConventions.SamplePublicVariable = "foo blah string2";						
-
-//-----------------------------------------------------------------
-//  A. Analysis of Naming Convention - Class External (Referring ONLY to outside of a class, its API, as shown above)	
-//
-//			1. RMC Convention 	 is **unique**   across each; public vs. static, method vs variable, non-constant vs constant.
-//			2. CSharp Convention is **the same** across each.
-//
-//-----------------------------------------------------------------
-
-
-//-----------------------------------------------------------------
-//  B. Analysis of Naming Convention - Class Internal (Referring ONLY to inside of a class)	
-//
-//			1. RMC Convention 	 is TBD.
-//			2. CSharp Convention is TBD.
-//
-//-----------------------------------------------------------------
-
-}
+		void Start () 
+		{
+			
+			//-----------------------------------------------------------------
+			//  1. RMC Naming Conventions
+			//
+			//			(See http://www.rivellomultimediaconsulting.com/c-sharp-coding-conventions-and-standards/ )
+			//
+			//-----------------------------------------------------------------
+			
+			// INSTANTIATE
+			//1.1
+			ClassWithRMCConventions classWithRMCConventions = new ClassWithRMCConventions();	
+			
+			// STATIC MEMBER
+			//1.2
+			ClassWithRMCConventions.SamplePublicStaticMethod("foo string1");					
+			//1.3
+			string s1 = ClassWithRMCConventions.SAMPLE_PUBLIC_STATIC_CONSTANT;					
+			
+			// INSTANCE MEMBER
+			//1.4
+			classWithRMCConventions.samplePublicMethod("foo string1");							
+			//1.5
+			classWithRMCConventions.samplePublicVariable = "foo blah string1";					
+			
+			
+			
+			//-----------------------------------------------------------------
+			//  2. C# Naming Conventions 	
+			//
+			//			(See http://msdn.microsoft.com/en-us/library/vstudio/ff926074.aspx)
+			//			(See http://msdn.microsoft.com/en-us/library/ms229045.aspx)
+			//
+			//-----------------------------------------------------------------
+			
+			// INSTANTIATE
+			//2.1
+			ClassWithCSharpConventions classWithCSharpConventions = new ClassWithCSharpConventions();	
+			
+			// STATIC MEMBER
+			//2.2
+			ClassWithCSharpConventions.SamplePublicStaticMethod("foo string2");							
+			//2.3
+			string s2 = ClassWithCSharpConventions.SamplePublicStaticConstant;							
+			
+			// INSTANCE MEMBER
+			//2.4
+			classWithCSharpConventions.SamplePublicMethod("foo string2");								
+			//2.5
+			classWithCSharpConventions.SamplePublicVariable = "foo blah string2";						
+			
+			//-----------------------------------------------------------------
+			//  A. Analysis of Naming Convention - Class External (Referring ONLY to outside of a class, its API, as shown above)	
+			//
+			//			1. RMC Convention 	 is **unique**   across each; public vs. static, method vs variable, non-constant vs constant.
+			//			2. CSharp Convention is **the same** across each.
+			//
+			//-----------------------------------------------------------------
+			
+			
+			//-----------------------------------------------------------------
+			//  B. Analysis of Naming Convention - Class Internal (Referring ONLY to inside of a class)	
+			//
+			//			1. RMC Convention 	 is TBD.
+			//			2. CSharp Convention is TBD.
+			//
+			//-----------------------------------------------------------------
+			
+		}
 		
 		// PUBLIC
 	
