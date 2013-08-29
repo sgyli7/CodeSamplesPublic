46549ef5472bbea02f229f1f9eaee8d38b6e9395
srivello <srivello@github.com>
2013-08-29 13:09:36 +0700

    Bowling Updated

---
+++ projects/UnityWithCocktails/Assets/UnityWithCocktails/Lessons/Lesson3.2_CSharp_2/C. Operator Overloading/Lesson32_CSharp_2_Operator_Overloading.cs
@@ -0,0 +1,152 @@
+﻿/**
+ * Copyright (C) 2005-2013 by Rivello Multimedia Consulting (RMC).                    
+ * code [at] RivelloMultimediaConsulting [dot] com                                                  
+ *                                                                      
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the      
+ * "Software"), to deal in the Software without restriction, including  
+ * without limitation the rights to use, copy, modify, merge, publish,  
+ * distribute, sublicense, and#or sell copies of the Software, and to   
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:                                            
+ *                                                                      
+ * The above copyright notice and this permission notice shall be       
+ * included in all copies or substantial portions of the Software.      
+ *                                                                      
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,      
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF   
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR    
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.                                      
+ */
+// Marks the right margin of code *******************************************************************
+
+
+//--------------------------------------
+//  Imports
+//--------------------------------------
+using UnityEngine;
+using System.Collections;
+
+//--------------------------------------
+//  Class
+//--------------------------------------
+public class Lesson32_CSharp_2_Operator_Overloading : MonoBehaviour 
+{
+
+	//--------------------------------------
+	//  Properties
+	//--------------------------------------
+	
+	// GETTER / SETTER
+	///<summary>
+	///	This is a sample getter/setter property.
+	///</summary>
+		
+	// PUBLIC
+	///<summary>
+	///	This is a sample public property.
+	///</summary>
+	
+	// PUBLIC STATIC
+	///<summary>
+	///	This is a sample public static property.
+	///</summary>
+	
+	// PRIVATE
+	///<summary>
+	///	This is a sample private property.
+	///</summary>
+	
+	// PRIVATE STATIC
+	///<summary>
+	///	This is a sample private static property.
+	///</summary>
+	
+	
+	//--------------------------------------
+	//  Methods
+	//--------------------------------------		
+	///<summary>
+	///	Use this for initialization
+	///</summary>
+	void Start () 
+	{
+		
+		//////////////////////////////////////////////////////////////////////////////
+		//
+		//	OVERLOADING OPERATORS -- MORE 'NATIVE' FUNCTIONALITY FOR YOUR CUSTOM CLASSES
+		//
+		//	LINK: http://www.tutorialspoint.com/csharp/csharp_operator_overloading.htm
+		//			
+		//	THESE CAN BE OVERLOADED
+		//	+, -, !, ~, ++, --	
+		//	+, -, *, /, %	
+		//	==, !=, <, >, <=, >=	
+		//
+		//	THESE *CANNOT* BE OVERLOADED
+		//	&&, ||
+		//	+=, -=, *=, /=, %=	
+		//	=, ., ?:, ->, new, is, sizeof, typeof	
+		//	
+		//////////////////////////////////////////////////////////////////////////////
+		
+		//	SETUP
+		BarTab sons_bartab 		= new BarTab (120.10f);
+		BarTab daughters_bartab = new BarTab (14.00f);
+		BarTab mothers_bartab 	= new BarTab ();
+		
+		//	TEST OUTPUT
+		Debug.Log ("sons_bartab		: " + sons_bartab);
+		Debug.Log ("daughters_bartab: " + daughters_bartab);
+		Debug.Log ("mothers_bartab	: " + mothers_bartab);
+		
+		//	TEST OPERATORS
+		mothers_bartab		+= sons_bartab;
+		mothers_bartab		+= daughters_bartab;
+		
+		//	TEST OUTPUT
+		Debug.Log ("sons_bartab		: " + sons_bartab);
+		Debug.Log ("daughters_bartab: " + daughters_bartab);
+		Debug.Log ("mothers_bartab	: " + mothers_bartab);
+		//
+		Debug.Log ("Greater		: " + (sons_bartab >  daughters_bartab)	);
+		Debug.Log ("GreaterEqual: " + (sons_bartab >= daughters_bartab)	);
+		Debug.Log ("Lesser		: " + (sons_bartab <  daughters_bartab)	);
+		Debug.Log ("LesserEqual: "  + (sons_bartab <= daughters_bartab)	);
+		Debug.Log ("Equals		: " + (sons_bartab == daughters_bartab)	);
+		Debug.Log ("NotEquals	: " + (sons_bartab != daughters_bartab)	);
+		
+		//	NOTE: THE MAGIC HERE IS THAT '+' IS USING A CUSTOM (NOT NATIVE) IMPLEMENTATION
+		//	EXCELLENT!
+		
+		
+	}
+	
+	
+	///<summary>
+	///	Called once per frame
+	///</summary>
+	void Update () 
+	{
+		
+		
+	}
+	
+	// PUBLIC
+	
+	// PUBLIC STATIC
+	
+	// PRIVATE
+	
+	// PRIVATE STATIC
+	
+	
+	//--------------------------------------
+	//  Events
+	//--------------------------------------
+
+}
+
---
+++ projects/UnityWithCocktails/Assets/UnityWithCocktails/Lessons/Lesson3.2_CSharp_2/C. Operator Overloading/Scripts/BarTab.cs
@@ -0,0 +1,226 @@
+﻿/**
+ * Copyright (C) 2005-2013 by Rivello Multimedia Consulting (RMC).                    
+ * code [at] RivelloMultimediaConsulting [dot] com                                                  
+ *                                                                      
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the      
+ * "Software"), to deal in the Software without restriction, including  
+ * without limitation the rights to use, copy, modify, merge, publish,  
+ * distribute, sublicense, and#or sell copies of the Software, and to   
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:                                            
+ *                                                                      
+ * The above copyright notice and this permission notice shall be       
+ * included in all copies or substantial portions of the Software.      
+ *                                                                      
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,      
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF   
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR    
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.                                      
+ */
+// Marks the right margin of code *******************************************************************
+
+
+//--------------------------------------
+//  Imports
+//--------------------------------------
+using UnityEngine;
+using System;
+
+//--------------------------------------
+//  Class
+//--------------------------------------
+public class BarTab 
+{
+
+	//--------------------------------------
+	//  Properties
+	//--------------------------------------
+	// GETTER / SETTER
+	///<summary>
+	///	ASSUME: That this value is the most important value exposed by the BarTab class. 
+	/// 
+	/// NOTE: We overload all operators to help us work with this value.
+	/// 
+	///</summary>
+	private float _totalValue_float;
+	public float totalValue 
+	{ 
+		get
+		{
+			return _totalValue_float;
+		}
+		set
+		{
+			_totalValue_float = value;
+			
+		}
+	}
+	
+	// PUBLIC
+	
+	// PUBLIC STATIC
+	
+	// PRIVATE
+	
+	
+	// PRIVATE STATIC
+	
+	//--------------------------------------
+	//  Methods
+	//--------------------------------------		
+	///<summary>
+	///	Constructor
+	//		This runs when "BarTab barTab = new BarTab();" is called
+	///</summary>
+	public BarTab (float aInitialTotalValue_float = 0) 
+	{
+		//Debug.Log ("BarTab.constructor()");
+		_totalValue_float = aInitialTotalValue_float;
+		
+	}
+	
+	///<summary>
+	///	Destructor
+	//		This runs when "barTab = null;" is called
+	///</summary>
+	~BarTab () 
+	{
+		//Debug.Log ("BarTab.destructor()");
+
+		
+	}
+	
+	
+		
+	// PUBLIC
+	
+	
+		
+	// PUBLIC STATIC
+	
+	///<summary>
+	///	OPERATOR OVERLOAD
+	///</summary>
+	public static BarTab operator + (BarTab aBarTab1, BarTab aBarTab2)
+	{
+	  BarTab barTab = new BarTab();
+	  barTab.totalValue = aBarTab1.totalValue + aBarTab2.totalValue;
+	  return barTab;
+	}
+	
+	///<summary>
+	///	OPERATOR OVERLOAD
+	///</summary>
+	public static BarTab operator - (BarTab aBarTab1, BarTab aBarTab2)
+	{
+	  BarTab barTab = new BarTab();
+	  barTab.totalValue = aBarTab1.totalValue - aBarTab2.totalValue;
+	  return barTab;
+	}
+	
+	
+	///<summary>
+	///	OPERATOR OVERLOAD
+	///</summary>
+	public static bool operator == (BarTab aBarTab1, BarTab aBarTab2)
+	{
+		bool equals_boolean = false;
+		if (aBarTab1.totalValue == aBarTab2.totalValue) {
+			equals_boolean = true;
+		}
+		return equals_boolean;
+	}
+	
+	
+	///<summary>
+	///	OPERATOR OVERLOAD
+	///</summary>
+	public static bool operator != (BarTab aBarTab1, BarTab aBarTab2)
+	{
+		bool equals_boolean = false;
+		if (aBarTab1.totalValue == aBarTab2.totalValue) {
+			equals_boolean = true;
+		}
+		return !equals_boolean;
+	}
+	
+	
+	
+	///<summary>
+	///	OPERATOR OVERLOAD
+	///</summary>
+	public static bool operator < (BarTab aBarTab1, BarTab aBarTab2)
+	{
+		bool lessThan_boolean = false;
+		if (aBarTab1.totalValue < aBarTab2.totalValue) {
+			lessThan_boolean = true;
+		}
+		return lessThan_boolean;
+	}
+	
+	
+	
+	///<summary>
+	///	OPERATOR OVERLOAD
+	///</summary>
+	public static bool operator > (BarTab aBarTab1, BarTab aBarTab2)
+	{
+		bool greaterThan_boolean = false;
+		if (aBarTab1.totalValue > aBarTab2.totalValue) {
+			greaterThan_boolean = true;
+		}
+		return greaterThan_boolean;
+	}
+	
+	
+	
+	///<summary>
+	///	OPERATOR OVERLOAD
+	///</summary>
+	public static bool operator <= (BarTab aBarTab1, BarTab aBarTab2)
+	{
+		bool lessThanOrEqualTo_boolean = false;
+		if (aBarTab1.totalValue <= aBarTab2.totalValue) {
+			lessThanOrEqualTo_boolean = true;
+		}
+		return lessThanOrEqualTo_boolean;
+	}
+	
+	
+	///<summary>
+	///	OPERATOR OVERLOAD
+	///</summary>
+	public static bool operator >= (BarTab aBarTab1, BarTab aBarTab2)
+	{
+		bool greaterThanOrEqualTo_boolean = false;
+		if (aBarTab1.totalValue >= aBarTab2.totalValue) {
+			greaterThanOrEqualTo_boolean = true;
+		}
+		return greaterThanOrEqualTo_boolean;
+	}
+	
+	
+	///<summary>
+	///	OPERATOR OVERLOAD
+	///</summary>
+	public override string ToString()
+	{
+		return "[BarTab "+ _totalValue_float.ToString("00.00")+" ]";
+	}
+	
+	// PRIVATE
+	
+	// PRIVATE STATIC
+	
+	// PRIVATE COROUTINE
+	
+	// PRIVATE INVOKE
+	
+	//--------------------------------------
+	//  Events
+	//--------------------------------------
+}
