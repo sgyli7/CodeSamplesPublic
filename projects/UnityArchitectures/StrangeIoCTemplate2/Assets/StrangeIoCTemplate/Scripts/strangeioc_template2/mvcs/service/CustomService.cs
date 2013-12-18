2e098423c251eaaa90cc86a2fb6456c53e781569
srivello <srivello@github.com>
2013-12-18 14:01:36 +0100

    Updated architecture project locations

---
+++ projects/UnityArchitectures/StrangeIoCTemplate2/Assets/StrangeIoCTemplate/Scripts/strangeioc_template2/mvcs/service/CustomService.cs
@@ -0,0 +1,139 @@
+﻿/**
+ * Copyright (C) 2005-2013 by Rivello Multimedia Consulting (RMC).                    
+ * code [at] RivelloMultimediaConsulting [dot] com                                                  
+ *                                                                      
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the      
+ * "Software"), to deal in the Software without restriction, including  
+ * without limitation the rights to use, copy, modify, merge, publish,  
+ * distribute, sublicense, and#or sell copies of the Software, and to   
+ * permit persons to whom the Software is furn
+ * 
+ * ished to do so, subject to
+ * the following conditions:                                            
+ *                                                                      
+ * The above copyright notice and this permission notice shall be       
+ * included in all copies or substantial portions of the Software.      
+ *                                                                      
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,      
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF   
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR    
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.                                      
+ */
+// Marks the right margin of code *******************************************************************
+
+//--------------------------------------
+//  Imports
+//--------------------------------------
+using UnityEngine;
+using System.Collections.Generic;
+using com.rmc.projects.strangeioc_template2.mvcs.mvc.controller.signals;
+
+//--------------------------------------
+//  Namespace
+//--------------------------------------
+namespace com.rmc.projects.strangeioc_template2.mvcs.service
+{
+	
+	//--------------------------------------
+	//  Namespace Properties
+	//--------------------------------------
+	
+	
+	//--------------------------------------
+	//  Class Attributes
+	//--------------------------------------
+	
+	
+	//--------------------------------------
+	//  Class
+	//--------------------------------------
+	public class CustomService : IService
+	{
+		
+		//--------------------------------------
+		//  Properties
+		//--------------------------------------
+		// GETTER / SETTER
+		[Inject]
+		public CustomServiceLoadedSignal customServiceLoadedSignal {get;set;}
+		
+		// PUBLIC
+		
+		// PUBLIC STATIC
+		
+		// PRIVATE
+		
+		// PRIVATE STATIC
+		
+		//--------------------------------------
+		//  Methods
+		//--------------------------------------
+		
+		
+		///////////////////////////////////////////////////////////////////////////
+		///////////////////////////////////////////////////////////////////////////
+		///			CONSTRUCTOR / DESTRUCTOR
+		///////////////////////////////////////////////////////////////////////////
+		///////////////////////////////////////////////////////////////////////////
+		///<summary>
+		///	 Constructor
+		///</summary>
+		public CustomService( )
+		{
+			//Debug.Log ("CustomService.constructor()");
+		}
+		
+		~CustomService()
+		{
+			
+		}
+		
+		
+		/// <summary>
+		/// Dos the load favorite videogames.
+		/// </summary>
+		public void doLoadFavoriteVideogames ()
+		{
+
+			Debug.Log ("CustomService.doLoadFavoriteVideogames()");
+
+
+			//
+			List<string> favoriteVideogamesList_string = new List<string>();
+			
+			// A SERVICE SHOULD LOAD DATA FROM AN EXTERNAL SOURCE (see "Resources" folder)
+			TextAsset textAsset = (TextAsset) Resources.Load("FavoriteVideogamesList", typeof(TextAsset));
+			
+			//CONVERT ARRAY TO LIST FOR EASIER USAGE
+			string[] favoriteVideogamesArray_string = textAsset.text.Split ("\n"[0]);
+			foreach (string s in favoriteVideogamesArray_string) {
+				favoriteVideogamesList_string.Add (s);
+			}
+			
+			//WHEN IT IS LOADED, SEND AN EVENT
+			customServiceLoadedSignal.Dispatch (favoriteVideogamesList_string);
+			
+			
+		}
+		
+		
+		
+		// PRIVATE
+		
+		// PRIVATE STATIC
+		
+		// PRIVATE COROUTINE
+		
+		// PRIVATE INVOKE
+		
+		//--------------------------------------
+		//  Events
+		//--------------------------------------
+		
+		
+	}
+}
