7674076b5f9aa695cb70a4b701cbe4b539fb5afb
srivello <srivello@github.com>
2014-01-07 13:05:57 -0500

    UnitySceneTest Project Added

---
+++ projects/UnityApplicantTests/UnitySceneTest/Assets/UnitySceneTest/Scripts/Standard/com/rmc/projects/unity_scene_test/GUIComponent.cs
@@ -0,0 +1,137 @@
+﻿/**
+ * Copyright (C) 2005-2013 by Rivello Multimedia Consulting (RMC).                    
+ * code [at] RivelloMultimediaConsulting [dot] com                                                  
+ *                                                                      
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the      
+ * "Software"), to deal in the Software without restriction, including  
+ * without limitation the rights to use, copy, modify, merge, publish,  
+ * distribute, sublicense, and#or sell copies of the Software, and to   
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:                                            
+ *                                                                      
+ * The above copyright notice and this permission notice shall be       
+ * included in all copies or substantial portions of the Software.      
+ *                                                                      
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,      
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF   
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR    
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.                                      
+ */
+// Marks the right margin of code *******************************************************************
+
+
+//--------------------------------------
+//  Imports
+//--------------------------------------
+using UnityEngine;
+
+//--------------------------------------
+//  Namespace
+//--------------------------------------
+namespace com.rmc.projects.unity_scene_test
+{
+	
+	//--------------------------------------
+	//  Namespace Properties
+	//--------------------------------------
+	
+	
+	//--------------------------------------
+	//  Class Attributes
+	//--------------------------------------
+	
+	
+	//--------------------------------------
+	//  Class
+	//--------------------------------------
+	public class GUIComponent : SuperMonoBehavior 
+	{
+		
+		//--------------------------------------
+		//  Attributes
+		//--------------------------------------
+		
+		
+		//--------------------------------------
+		//  Properties
+		//--------------------------------------
+		
+		// GETTER / SETTER
+		///<summary>
+		///	This is a sample getter/setter property.
+		///</summary>
+		private bool _buttonsEnabled_boolean;
+		public bool buttonsEnabled { 
+			get 
+			{ 
+				return _buttonsEnabled_boolean; 
+			}
+			set 
+			{ 
+				_buttonsEnabled_boolean = value; 
+			}
+		}
+
+		
+		// PUBLIC
+		
+		// PUBLIC STATIC
+		
+		// PRIVATE
+		
+		// PRIVATE STATIC
+		
+		//--------------------------------------
+		//  Methods
+		//--------------------------------------		
+		///<summary>
+		///	Use this for initialization
+		///</summary>
+		void Start () 
+		{
+			
+			
+		}
+		
+		
+		///<summary>
+		///	Called once per frame
+		///</summary>
+		void Update () 
+		{
+			
+			//Debug.Log("Update ()");
+			
+		}
+		
+		// PUBLIC
+		
+		// PUBLIC STATIC
+		
+		// PRIVATE
+		
+		// PRIVATE STATIC
+		
+		//--------------------------------------
+		//  Events
+		//--------------------------------------
+		private void OnGUI() 
+		{
+
+			float middleX_float = Screen.width/2;
+			float middleYAdjusted_float = Screen.height - 20;
+
+			//Debug.Log ("GUIComponent.OnGUI()");
+			if (_buttonsEnabled_boolean) {
+				if (GUI.Button (new Rect (middleX_float - 50, middleYAdjusted_float - 15, 100, 30), "Go Back")){
+					simpleControllerComponent.changeCameraMode (CameraMode.CAMERA_MODE_MAIN);
+				}
+			}
+			
+		}
+	}
+}
---
+++ projects/UnityApplicantTests/UnitySceneTest/Assets/UnitySceneTest/Scripts/Standard/com/rmc/projects/unity_scene_test/SimpleControllerComponent.cs
@@ -0,0 +1,155 @@
+﻿/**
+ * Copyright (C) 2005-2013 by Rivello Multimedia Consulting (RMC).                    
+ * code [at] RivelloMultimediaConsulting [dot] com                                                  
+ *                                                                      
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the      
+ * "Software"), to deal in the Software without restriction, including  
+ * without limitation the rights to use, copy, modify, merge, publish,  
+ * distribute, sublicense, and#or sell copies of the Software, and to   
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:                                            
+ *                                                                      
+ * The above copyright notice and this permission notice shall be       
+ * included in all copies or substantial portions of the Software.      
+ *                                                                      
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,      
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF   
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR    
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.                                      
+ */
+// Marks the right margin of code *******************************************************************
+
+
+//--------------------------------------
+//  Imports
+//--------------------------------------
+using UnityEngine;
+using com.rmc.exceptions;
+
+//--------------------------------------
+//  Namespace
+//--------------------------------------
+namespace com.rmc.projects.unity_scene_test
+{
+	
+	//--------------------------------------
+	//  Namespace Properties
+	//--------------------------------------
+	public enum CameraMode
+	{
+		CAMERA_MODE_MAIN,
+		CAMERA_MODE_ZOOM
+
+	}
+	
+	
+	//--------------------------------------
+	//  Class Attributes
+	//--------------------------------------
+	
+	
+	//--------------------------------------
+	//  Class
+	//--------------------------------------
+	public class SimpleControllerComponent : MonoBehaviour 
+	{
+		
+		//--------------------------------------
+		//  Attributes
+		//--------------------------------------
+		
+		
+		//--------------------------------------
+		//  Properties
+		//--------------------------------------
+		
+		// GETTER / SETTER
+
+		// PUBLIC
+		/// <summary>
+		/// The main camera.
+		/// </summary>
+		public Camera mainCamera;
+		
+		/// <summary>
+		/// The zoom camera.
+		/// </summary>
+		public Camera zoomCamera;
+
+		/// <summary>
+		/// The GUI component.
+		/// </summary>
+		public GUIComponent guiComponent;
+
+
+
+		// PUBLIC STATIC
+		
+		// PRIVATE
+		
+		// PRIVATE STATIC
+		
+		//--------------------------------------
+		//  Methods
+		//--------------------------------------		
+		///<summary>
+		///	Use this for initialization
+		///</summary>
+		void Start () 
+		{
+			changeCameraMode (CameraMode.CAMERA_MODE_MAIN);
+			
+		}
+		
+		
+		///<summary>
+		///	Called once per frame
+		///</summary>
+		void Update () 
+		{
+			
+			//Debug.Log("Update ()");
+			
+		}
+		
+		// PUBLIC
+		/// <summary>
+		/// Changes the camera mode.
+		/// </summary>
+		/// <param name="aCameraMode">A camera mode.</param>
+		public void changeCameraMode (CameraMode aCameraMode)
+		{
+			switch (aCameraMode) {
+				case CameraMode.CAMERA_MODE_MAIN:
+					mainCamera.enabled = true;
+					zoomCamera.enabled = false;
+					guiComponent.buttonsEnabled = false;
+					break;
+				case CameraMode.CAMERA_MODE_ZOOM:
+					mainCamera.enabled = false;
+					zoomCamera.enabled = true;
+					guiComponent.buttonsEnabled = true;
+					break;
+				default:
+					throw new SwitchStatementException();
+					break;
+			}
+
+		}
+		
+		
+		// PUBLIC STATIC
+		
+		// PRIVATE
+		
+		// PRIVATE STATIC
+		
+		//--------------------------------------
+		//  Events
+		//--------------------------------------
+	}
+}
