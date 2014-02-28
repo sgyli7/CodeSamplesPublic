2c5868dd2fcda6739936979072567538789d7655
Samuel Asher Rivello <code@RivelloMultimediaConsulting.com>
2014-02-27 22:28:18 -0500

    Update

--- projects/UnityArchitectures/StrangeIoCTemplate2/Assets/StrangeIoCTemplate/Documentation/CodeSnippetsForBlogArticle.cs
+++ projects/UnityArchitectures/StrangeIoCTemplate2/Assets/StrangeIoCTemplate/Documentation/CodeSnippetsForBlogArticle.cs
@@ -1,15 +1,77 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿/*
+ * 
 
-public class CodeSnippetsForBlogArticle : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+
+1. CONTEXT - SETUP BINDING
+
+commandBinder.Bind<GameListPropertyChangeSignal>().To<GameListPropertyChangeCommmand>();
+
+2. COMMAND - HANDLE BINDING
+
+public override void Execute()
+{
+
+	switch (propertyChangeSignalVO.propertyChangeType) {
+
+	case PropertyChangeType.CLEAR:
+		//ASK TO CLEAR THE MODEL
+		iCustomModel.doClearGameList(); 
+		break;
+	case PropertyChangeType.UPDATE:
+		//ASK TO UPDATE A VALUE IN THE MODEL
+		iCustomModel.gameList = propertyChangeSignalVO.value as List<string>;
+		break;
+	case PropertyChangeType.UPDATED:
+		//FOR THIS PROJECT, THE VIEW LISTENS DIRECTLY TO 'UPDATED'
+		//OPTIONALLY, WE COULD ALSO DO SOMETHING HERE IF NEEDED
+		break;
+	case PropertyChangeType.REQUEST:
+		//FORCE THE MODEL TO RE-SEND 'UPDATED' (WITH NO CHANGE)
+		//THIS IS VERY COMMON IN APPS (E.G. A TEMPORARY A DIALOG PROMPT)
+		iCustomModel.doRefreshGameList();
+		break;
+	default:
+		#pragma warning disable 0162
+		throw new SwitchStatementException(propertyChangeSignalVO.propertyChangeType.ToString());
+		break;
+		#pragma warning restore 0162
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+}
+
+
+3. MODEL - DISPATCH CHANGES
+
+private List<string> _gameList;
+public List<string> gameList 
+{
+	get 
+	{
+		return _gameList;
+	}
+	set 
+	{
+		//TODO: CONSIDER ALTERNATIVE THAT CHECKS "_gameList != value" BEFORE DISPATCHING
+		_gameList = value;
+		gameListPropertyChangeSignal.Dispatch (new PropertyChangeSignalVO(PropertyChangeType.UPDATED, _gameList) );
 	}
 }
+
+
+4. VIEW - HANDLE CHANGES
+
+private void _onGameListPropertyChangeSignal (PropertyChangeSignalVO aPropertyChangeSignalVO)
+{
+	if (aPropertyChangeSignalVO.propertyChangeType == PropertyChangeType.UPDATED) {
+
+		doRenderLayout(aPropertyChangeSignalVO.value as List<string>);
+	}
+}
+
+
+
+ * 
+ * 
+ **/
